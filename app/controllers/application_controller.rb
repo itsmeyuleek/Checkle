c8b8209c93bbbd906def698568599274c97aff5f
alisagrdva <alisagrdva@gmail.com>
2019-03-22 17:20:56 +0300

    New design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-#  before_action :authenticate_user!
-#  load_and_authorize_resource
-def add_user_id(action_controller_params)
+  #  before_action :authenticate_user!
+  #  load_and_authorize_resource
+  def add_user_id(action_controller_params)
     params = action_controller_params
     params[:user_id] = current_user.id
     params
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     @topics = Topic.all
     @cards = Card.all
+    @card = Card.first
   end
 
 end
